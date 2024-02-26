059c04aa3921f7d6cbe6e8fcb618017ec6b036f5
andero-magi <158137801+andero-magi@users.noreply.github.com>
2024-02-26 13:35:47 +0000

    Token type stuff

--- src/tokens.ts
+++ src/tokens.ts
@@ -18,19 +18,42 @@ export class FileLocation {
 }
 
 export enum TokenType {
-  EOF,
-  ERR,
-  ID,
-  NUM,
-  LPAREN,
-  RPAREN,
-  MUL,
-  POW,
-  DIV,
-  ADD,
-  SUB,
-  ABS_WALL,
-  COMMA,
+  EOF,      // End-Of-File
+  ERR,      // Unrecognized token
+  ID,       // [a-zA-Z_$][a-zA-Z0-9_$]*
+  NUM,      // [0-9]+(.[0-9]+)?(e[+-]?[0-9]+)?
+  LPAREN,   // (
+  RPAREN,   // )
+  MUL,      // *
+  POW,      // *
+  DIV,      // /
+  FLOORDIV, // //
+  ADD,      // + 
+  SUB,      // -
+  ABS_WALL, // |
+  COMMA,    // ,
+}
+
+function ttToString(type: TokenType): string {
+  switch(type) {
+    case TokenType.EOF:       return "<EOF>"
+    case TokenType.ERR:       return "UNKNOWN_TOKEN"
+    case TokenType.ID:        return "IDENTIFIER"
+    case TokenType.NUM:       return "NUMBER"
+    case TokenType.LPAREN:    return "LEFT_PAREN:'('"
+    case TokenType.RPAREN:    return "RIGHT_PAREN:')'"
+    case TokenType.MUL:       return "MUL:'*'"
+    case TokenType.POW:       return "POW:'**'"
+    case TokenType.DIV:       return "DIV:'/'"
+    case TokenType.FLOORDIV:  return "FLOORDIV:'//'"
+    case TokenType.ADD:       return "ADD:'+'"
+    case TokenType.SUB:       return "SUB:'-'"
+    case TokenType.ABS_WALL:  return "ABS_WALL:'|'"
+    case TokenType.COMMA:     return "COMMA:','"
+
+    default:
+      return "Unknown token type"
+  }
 }
 
 type TokenValue = string | number | null
