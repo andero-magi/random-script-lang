5bfd676c499b9648ce369611cfcab0fbec6cc9b8
Opilane TTHK <opilane@tthk.ee>
2024-05-21 13:09:25 +0300

    update

--- src/ast.ts
+++ src/ast.ts
@@ -3,6 +3,16 @@ import { FileLocation } from "./tokens"
 export interface NodeVisitor<C, R> {
   
   visitNumLiteral(expr: NumberLiteral, ctx: C): R
+
+  visitString(expr: StringLiteral, ctx: C): R
+
+  visitBoolean(expr: BooleanLiteral, ctx: C): R
+
+  visitId(expr: Identifier, ctx: C): R
+
+  visitBinary(expr: BinaryExpr, ctx: C): R
+
+  visitUnary(expr: UnaryExpr, ctx: C): R
 }
 
 export interface Node {
@@ -15,6 +25,24 @@ export interface Expr extends Node {
 
 }
 
+export class Identifier implements Expr {
+  value: string
+  location: FileLocation
+
+  visit<C, R>(visitor: NodeVisitor<C, R>, ctx: C): R {
+    return visitor.visitId(this, ctx)
+  }
+}
+
+export class StringLiteral implements Expr {
+  value: string
+  location: FileLocation
+
+  visit<C, R>(visitor: NodeVisitor<C, R>, ctx: C): R {
+    return visitor.visitString(this, ctx)
+  }
+}
+
 export class NumberLiteral implements Expr {
    location: FileLocation
    value: number
@@ -22,5 +50,54 @@ export class NumberLiteral implements Expr {
   visit<C, R>(visitor: NodeVisitor<C, R>, ctx: C): R {
     return visitor.visitNumLiteral(this, ctx)
   }
+}
 
+export class BooleanLiteral implements Expr {
+  location: FileLocation
+  value: boolean
+
+  visit<C, R>(visitor: NodeVisitor<C, R>, ctx: C): R {
+      return visitor.visitBoolean(this, ctx)
+  }
+}
+
+export enum BinaryOperation {
+  ADD,
+  SUB,
+  DIV,
+  MUL,
+  POW,
+  LT,
+  GT,
+  LTE,
+  GTE,
+  EQ,
+  NEQ
+}
+
+export class BinaryExpr implements Expr {
+  location: FileLocation
+  lhs: Expr
+  rhs: Expr
+  op: BinaryOperation
+
+  visit<C, R>(visitor: NodeVisitor<C, R>, ctx: C): R {
+      return visitor.visitBinary(this, ctx)
+  }
+}
+
+export enum UnaryOperation {
+  POS,
+  NEG,
+  ABS,
+}
+
+export class UnaryExpr implements Expr {
+  location: FileLocation
+  expr: Expr
+  op: UnaryOperation
+
+  visit<C, R>(visitor: NodeVisitor<C, R>, ctx: C): R {
+      return visitor.visitUnary(this, ctx)
+  }
 }
